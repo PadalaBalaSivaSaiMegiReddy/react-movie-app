f47cdfc2b7fb77b232ad1d19669c242a38cbec0f
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-07 12:26:31 +0530

    Add dependencies and update styles in client package

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../../components/navbar/NavBar'
+import './Home.scss'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <NavBar/>
+    </div>
+  )
+}
