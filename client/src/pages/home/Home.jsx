de36dbc01eb0a861647c4b9f2d6f70a06ed55a2a
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-09 11:48:44 +0530

    Add Featured component to Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../../components/featured/Featured'
 import NavBar from '../../components/navbar/NavBar'
 import './Home.scss'
 
@@ -5,10 +6,7 @@ export default function Home() {
   return (
     <div className='home'>
       <NavBar/>
-      <img width="100%"
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt="profile-img"
-          />
+      <Featured type="movie"/>
     </div>
   )
 }
