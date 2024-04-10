08cb6fe5a872ad9c3a5317e1bba58c56ed727757
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-10 21:58:06 +0530

    Update App component to include Login and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.scss";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 export default function App() {
   return (
     <div>
-      <Watch/>
+      {/* <Home/> */}
+      {/* <Watch/> */}
+      {/* <Register/> */}
+      <Login/>
     </div>
   )
 }
