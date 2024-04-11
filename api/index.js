c8722a991ed715ec32c8b462b42480b06366d765
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-11 12:12:06 +0530

    add bcrypt and hash password

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
-app.listen("3000",()=>{
-    console.log("Server running");
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to MongoDB");
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening to ${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log(err);
 })
+
+app.use("/api/auth",authRoute);
---
+++ api/routes/auth.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+//REGISTER
+router.post("/register", async (request, response) => {
+    try {
+
+        //gen hashed password
+
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(request.body.password,salt);
+
+  const newUser = new User({
+    username: request.body.username,
+    email: request.body.email,
+    password: hashedPassword,
+  });
+    const user = await newUser.save();
+    response.status(201).json(user);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+});
+
+//LOGIN
+
+module.exports = router;
