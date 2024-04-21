ec427ab5c9002fa090436d9ed898b1d7c078a5d8
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-21 11:53:58 +0530

    Refactor CRUD routes for lists and update authentication checks

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -11,32 +11,13 @@ router.post("/", verify, async (req, res) => {
     try {
       const savedList = await newList.save();
       res.status(201).json(savedList);
-    } catch (error) {
-      res.status(500).json(error);
+    } catch (err) {
+      res.status(500).json(err);
     }
   } else {
-    res.status(403).json("You are not allowed to add a list");
+    res.status(403).json("You are not allowed!");
   }
 });
-
-
-//UPDATE
-
-router.put("/:id", verify, async (req, res) => {
-  if (req.user.isAdmin) {
-    try {
-      const updatedList = await List.findByIdAndUpdate(
-        req.params.id,
-        { $set: req.body },
-        { new: true }
-      );
-      res.status(200).json(updatedList);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  }
-}
-);
 
 //DELETE
 
@@ -44,52 +25,41 @@ router.delete("/:id", verify, async (req, res) => {
   if (req.user.isAdmin) {
     try {
       await List.findByIdAndDelete(req.params.id);
-      res.status(200).json("The list has been deleted...");
-    } catch (error) {
-      res.status(500).json(error);
+      res.status(201).json("The list has been delete...");
+    } catch (err) {
+      res.status(500).json(err);
     }
+  } else {
+    res.status(403).json("You are not allowed!");
   }
-}
-);
+});
 
 //GET
 
-router.get("/find/:id", verify, async (req, res) => {
-  try {
-    const list = await List.findById(req.params.id);
-    res.status(200).json(list);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-}
-);
-
-//GET RANDOM
-
-router.get("/random", verify, async (req, res) => {
-  const type = req.query.type;
+router.get("/", verify, async (req, res) => {
+  const typeQuery = req.query.type;
+  const genreQuery = req.query.genre;
   let list = [];
   try {
-    if (type === "movie") {
-      list = await List.aggregate([
-        { $match: { type: "movie" } },
-        { $sample: { size: 10 } },
-      ]);
+    if (typeQuery) {
+      if (genreQuery) {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery, genre: genreQuery } },
+        ]);
+      } else {
+        list = await List.aggregate([
+          { $sample: { size: 10 } },
+          { $match: { type: typeQuery } },
+        ]);
+      }
     } else {
-      list = await List.aggregate([
-        { $match: { type: "series" } },
-        { $sample: { size: 10 } },
-      ]);
+      list = await List.aggregate([{ $sample: { size: 10 } }]);
     }
     res.status(200).json(list);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
-}
-);
-
-
-
-
+});
 
 module.exports = router;
